26a40ffb97a3b3ca81c1bf46699e44884bfb78af
CoderUtk <utkarshg08@gmail.com>
2017-01-14 12:34:40 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
             a[i]=sc.nextInt();
         System.out.println("Sorted Array is:\t");
         for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1;j++){
+            for(int j=i;j<n-1;j++){
                 if(a[j]>a[j+1]){
                     a[j+1]=a[j+1]+a[j];
                     a[j]=a[j+1]-a[j];
