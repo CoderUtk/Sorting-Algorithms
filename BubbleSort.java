4ce77c52ea524e006bf1dc43ebfc486277423ce5
CoderUtk <utkarshg08@gmail.com>
2017-01-15 12:59:40 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,15 +1,14 @@
 public class BubbleSort {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);
         System.out.println("Enter length of array:");
-        int n=sc.nextInt(),min,index=0;
-        int a[]=new int[1000];
+        int n=sc.nextInt();
+        int a[]=new int[n];
         System.out.println("Enter array:");
         for(int i=0;i<n;i++)
             a[i]=sc.nextInt();
         System.out.println("Sorted Array is:\t");
         for(int i=0;i<n-1;i++){
-            for(int j=i;j<n-1;j++){
+            for(int j=0;j<n-i-1;j++){
                 if(a[j]>a[j+1]){
                     a[j+1]=a[j+1]+a[j];
                     a[j]=a[j+1]-a[j];
