dce943a2ce13bc6e205cfbd42431b08c061b3e59
CoderUtk <utkarshg08@gmail.com>
2017-01-15 13:05:52 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -10,9 +10,9 @@ public class BubbleSort {
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
                 if(a[j]>a[j+1]){
-                    a[j+1]=a[j+1]+a[j];
-                    a[j]=a[j+1]-a[j];
-                    a[j+1]=a[j+1]-a[j];
+                    int temp= a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
                 }
             }
             
